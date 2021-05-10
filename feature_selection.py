8d971f3ea75364057512e11ed66d588ad8c0d222
TamaraRigo <83221534+TamaraRigo@users.noreply.github.com>
2021-05-10 10:11:52 +0300

    Update feature_selection.py

--- feature_selection.py
+++ feature_selection.py
@@ -4,8 +4,6 @@ from matplotlib import pyplot
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score as acc
 from sklearn.linear_model import LogisticRegression
@@ -20,25 +18,13 @@ import statsmodels.api as sm
 from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
 import statsmodels.api as sm
 
-data = pd.read_csv('original_data.csv', sep=';')
-data.head()
 
-# drop features
-data.drop(['Case_Number', 'PSQI_component_1', 'Daytime_Dozing', 'Start_time'], axis=1, inplace=True)
-
-# categorical features one hot encoding
-one_hot_features = ['University', 'Department_Name', 'Start_time_code', 'Daytime_Dozing_Groups']
-one_hot = pd.get_dummies(data, columns = one_hot_features, prefix=one_hot_features)
-
-# features for min-max normalisation
-scaler = MinMaxScaler()
-features = ['Sex', 'Avg_Weekly_Sleep_Duration', 'Avg_Sleep_Working_days', 'Avg_sleep_free_days', 'Cigarettes_dichotomous', 'Alcohol_dichotomous', 'Caffeine_dichotomous', 'coffee_dichotomous']
-one_hot[features] =scaler.fit_transform(one_hot[features])
-scaled_features_df = one_hot
+# import data
+data = pd.read_csv('data/scaled_data.csv', sep=';')
 
 # Split Data to Training and Test set
-y = scaled_features_df['psqi_2_groups']
-X = scaled_features_df.drop(['psqi_2_groups'], axis=1)
+y = scaled_data['psqi_2_groups']
+X = scaled_data.drop(['psqi_2_groups'], axis=1)
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.30, random_state=42)
 
 #STEPWISE SELECTION 
